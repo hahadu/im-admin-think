5ed5f6c65384e847ed1fdc95e8137d7799c6a7fc
hahadu <mhyzhu@163.com>
2020-11-30 00:30:14 +0800

    update

--- src/model/UserBaseModel.php
+++ src/model/UserBaseModel.php
@@ -23,5 +23,19 @@ class UserBaseModel extends BaseModel{
     use SoftDelete;
     protected $deleteTime = 'delete_time';
     protected $defaultSoftDelete = NULL;
+    protected $autoWriteTimestamp = true;
+    protected $updateTime = 'last_login_time';
+    protected $createTime = 'register_time';
+
+    /****
+     * 获取注册用户列表
+     * @return \think\Collection
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function get_user_list(){
+        return $this->field('id,username,avatar,email,phone,status,register_time,last_login_ip,last_login_time')->order('id','desc')->select();
+    }
 
 }
