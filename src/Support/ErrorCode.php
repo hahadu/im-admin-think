698db3a6766ce5d4a794e17746bffce47b27cba8
hahadu <mhyzhu@163.com>
2021-11-21 03:10:54 +0800

    update

--- src/Support/ErrorCode.php
+++ src/Support/ErrorCode.php
@@ -3,13 +3,27 @@
 namespace Hahadu\ThinkAdmin\Support;
 use ReflectionClass;
 use ReflectionClassConstant;
-
+use Hahadu\Reflector\Reflection;
 class ErrorCode
 {
+    /** 成功 */
+    const SUCCESS = 200;
     /** 失败 */
     const FAIL = 10000;
 
     public function getErrorCodeMessage($code = self::FAIL){
+        if($code == self::SUCCESS){
+            return "success";
+        }
+        $classDoc = new ReflectionClass(new self());
+        $list = array_flip($classDoc->getConstants());
+        $constant = new ReflectionClassConstant(new self(),$list[$code]);
+
+        $doc = new Reflection($constant);
+        $text = $doc->getContext();
+        return $text;
+
+
 
     }
 
