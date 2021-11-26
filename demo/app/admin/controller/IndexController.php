13703d69432ea78510dd2b51f57d461a6c0a1c97
hahadu <mhyzhu@163.com>
2021-11-27 03:33:44 +0800

    create modes

--- demo/app/admin/controller/IndexController.php
+++ demo/app/admin/controller/IndexController.php
@@ -18,21 +18,33 @@
 declare (strict_types = 1);
 
 namespace app\admin\controller;
+use app\admin\extend\AdminPageStyle;
 use Hahadu\Helper\StringHelper;
 use Hahadu\ThinkAdmin\controller\AdminBaseController;
+use Hahadu\ThinkAdmin\Layout\Content;
+use Hahadu\ThinkAdmin\model\AdminNav;
+use Hahadu\ThinkAdmin\Support\ErrorCode;
 use think\facade\View;
+use Hahadu\ThinkAdmin\Facades\Admin;
+use think\facade\Request;
+
 class IndexController extends AdminBaseController
 {
     public function initialize(){
         parent::initialize();
     }
-    public function index()
+    public function _index(Content $content)
     {
         View::assign('title','后台首页');
+        //    dump(Admin::user()->toArray());
         return view('index/index_adminLte');
     }
-    public function welcome(){
-        return '';
+    public function index(Content $content){
+
+        return $content->title('后台首页')
+            ->view('sss')
+            ->render();
+
     }
     public function jwt(){
         return new Builder();
